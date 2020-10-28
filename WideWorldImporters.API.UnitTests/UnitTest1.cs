edf15ef36537330f41b5eb9337d035317627aa55
Dixita <dixitabhanderi101@gmail.com>
2020-10-27 21:21:21 -0400

    demo and test run ready

---
+++ WideWorldImporters.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using System.Threading.Tasks;
+
+namespace WideWorldImporters.API.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public Task Test1()
+        {
+
+        }
+    }
+}
